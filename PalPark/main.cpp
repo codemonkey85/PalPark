ff2ea761d770079dc4fa0e953cbec5e5dec16868
Michael Bond <michael.bond.85@icloud.com>
2016-05-05 12:19:40 -0400

    Convert species from Gen III -> Gen IV

--- PalPark/main.cpp
+++ PalPark/main.cpp
@@ -1,11 +1,23 @@
 #include "include\pk3.h"
 #include "include\pk4.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 	if (argc <= 1)
 	{
-		return 0;
+		//return 0;
 	}
+
+	map<GBASpecies::gen_3_species, Species::gen_4_species> gen_3_species_to_gen_4_species = create_map();
+
+	auto thing = GBASpecies::deoxys;
+
+	cout << thing << '\n';
+	cout << gen_3_species_to_gen_4_species[thing] << '\n';
+
+	string test;
+	cin >> test;
 	return 0;
 }
